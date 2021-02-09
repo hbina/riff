09fdb64bdc4b0df7f83d1aa1fac7d3c30c1c16ea
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2021-02-10 00:06:49 +0800

    Removed unused mut
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -8,7 +8,7 @@ use riffu::{
 
 fn parse_file(_: ()) {
     {
-        let mut chunk_root = ChunkDisk::from_path("test_assets/set_4.riff").unwrap();
+        let chunk_root = ChunkDisk::from_path("test_assets/set_4.riff").unwrap();
         {
             assert_eq!(chunk_root.payload_len().unwrap(), 102);
             assert_eq!(chunk_root.id().unwrap().as_bytes(), RIFF_ID);
@@ -16,12 +16,12 @@ fn parse_file(_: ()) {
             assert_eq!(chunk_root.iter().unwrap().fold(0, |acc, _| acc + 1), 2);
         }
         {
-            let mut list_1 = chunk_root.iter().unwrap().next().unwrap().unwrap();
+            let list_1 = chunk_root.iter().unwrap().next().unwrap().unwrap();
             assert_eq!(list_1.id().unwrap().as_bytes(), LIST_ID);
             assert_eq!(list_1.chunk_type().unwrap().as_bytes(), b"tst1");
             assert_eq!(list_1.iter().unwrap().fold(0, |acc, _| acc + 1), 2);
             {
-                let mut test = list_1.iter().unwrap().next().unwrap().unwrap();
+                let test = list_1.iter().unwrap().next().unwrap().unwrap();
                 assert_eq!(test.id().unwrap().as_bytes(), b"test");
                 assert_eq!(
                     test.get_raw_child().unwrap(),
@@ -29,7 +29,7 @@ fn parse_file(_: ()) {
                 );
             }
             {
-                let mut test = list_1.iter().unwrap().skip(1).next().unwrap().unwrap();
+                let test = list_1.iter().unwrap().skip(1).next().unwrap().unwrap();
                 assert_eq!(test.id().unwrap().as_bytes(), b"test");
                 assert_eq!(
                     test.get_raw_child().unwrap(),
@@ -38,7 +38,7 @@ fn parse_file(_: ()) {
             }
         }
         {
-            let mut list_1 = chunk_root.iter().unwrap().skip(1).next().unwrap().unwrap();
+            let list_1 = chunk_root.iter().unwrap().skip(1).next().unwrap().unwrap();
             assert_eq!(list_1.id().unwrap().as_bytes(), b"seqt");
             assert_eq!(list_1.iter().unwrap().fold(0, |acc, _| acc + 1), 1);
             assert_eq!(
@@ -67,7 +67,7 @@ fn parse_file(_: ()) {
         }
     }
     {
-        let mut chunk_root = ChunkDisk::from_path("test_assets/set_3.riff").unwrap();
+        let chunk_root = ChunkDisk::from_path("test_assets/set_3.riff").unwrap();
         {
             assert_eq!(chunk_root.payload_len().unwrap(), 100);
             assert_eq!(chunk_root.id().unwrap().as_bytes(), RIFF_ID);
@@ -75,12 +75,12 @@ fn parse_file(_: ()) {
             assert_eq!(chunk_root.iter().unwrap().fold(0, |acc, _| acc + 1), 2);
         }
         {
-            let mut list_1 = chunk_root.iter().unwrap().next().unwrap().unwrap();
+            let list_1 = chunk_root.iter().unwrap().next().unwrap().unwrap();
             assert_eq!(list_1.id().unwrap().as_bytes(), LIST_ID);
             assert_eq!(list_1.chunk_type().unwrap().as_bytes(), b"tst1");
             assert_eq!(list_1.iter().unwrap().fold(0, |acc, _| acc + 1), 2);
             {
-                let mut test = list_1.iter().unwrap().next().unwrap().unwrap();
+                let test = list_1.iter().unwrap().next().unwrap().unwrap();
                 assert_eq!(test.id().unwrap().as_bytes(), b"test");
                 assert_eq!(
                     test.get_raw_child().unwrap(),
@@ -88,7 +88,7 @@ fn parse_file(_: ()) {
                 );
             }
             {
-                let mut test = list_1.iter().unwrap().skip(1).next().unwrap().unwrap();
+                let test = list_1.iter().unwrap().skip(1).next().unwrap().unwrap();
                 assert_eq!(test.id().unwrap().as_bytes(), b"test");
                 assert_eq!(
                     test.get_raw_child().unwrap(),
@@ -97,7 +97,7 @@ fn parse_file(_: ()) {
             }
         }
         {
-            let mut list_1 = chunk_root.iter().unwrap().skip(1).next().unwrap().unwrap();
+            let list_1 = chunk_root.iter().unwrap().skip(1).next().unwrap().unwrap();
             assert_eq!(list_1.id().unwrap().as_bytes(), b"seqt");
             assert_eq!(list_1.iter().unwrap().fold(0, |acc, _| acc + 1), 1);
             assert_eq!(
@@ -126,7 +126,7 @@ fn parse_file(_: ()) {
         }
     }
     {
-        let mut chunk_root = ChunkDisk::from_path("test_assets/set_2.riff").unwrap();
+        let chunk_root = ChunkDisk::from_path("test_assets/set_2.riff").unwrap();
         assert_eq!(chunk_root.payload_len().unwrap(), 24);
         assert_eq!(chunk_root.id().unwrap().as_bytes(), b"RIFF");
         assert_eq!(chunk_root.chunk_type().unwrap().as_bytes(), b"smpl");
@@ -136,14 +136,14 @@ fn parse_file(_: ()) {
             expected_content.len()
         );
         for (chunk, (name, data)) in chunk_root.iter().unwrap().zip(expected_content) {
-            let mut chunk = chunk.unwrap();
+            let chunk = chunk.unwrap();
             assert_eq!(chunk.id().unwrap().as_bytes(), name);
             assert_eq!(chunk.get_raw_child().unwrap().len(), data.len());
             assert_eq!(chunk.get_raw_child().unwrap(), data);
         }
     }
     {
-        let mut chunk_root = ChunkDisk::from_path("test_assets/set_1.riff").unwrap();
+        let chunk_root = ChunkDisk::from_path("test_assets/set_1.riff").unwrap();
         assert_eq!(chunk_root.payload_len().unwrap(), 14);
         assert_eq!(chunk_root.id().unwrap().as_bytes(), b"RIFF");
         assert_eq!(chunk_root.chunk_type().unwrap().as_bytes(), b"smpl");
@@ -153,7 +153,7 @@ fn parse_file(_: ()) {
             expected_content.len()
         );
         for (chunk, expected) in chunk_root.iter().unwrap().zip(expected_content) {
-            let mut chunk = chunk.unwrap();
+            let chunk = chunk.unwrap();
             assert_eq!(chunk.get_raw_child().unwrap().len(), expected.len());
             assert_eq!(chunk.get_raw_child().unwrap(), expected);
         }
